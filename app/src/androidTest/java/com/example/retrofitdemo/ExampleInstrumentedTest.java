488bfbde4d620a26fe3729419120eacf7eacf062
jitesh_p <jitesh.p@msp-group.co.uk>
2019-03-28 14:20:28 +0530

    New Commit

--- app/src/androidTest/java/com/example/retrofitdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/retrofitdemo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.msp.retrofitdemo", appContext.getPackageName());
+        assertEquals("com.example.retrofitdemo", appContext.getPackageName());
     }
 }
